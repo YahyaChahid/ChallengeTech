78b62c7bf966bc413f777de4912d3b5f66535db8
YahyaChahid <chahid.yahya@emsi-edu.ma>
2024-03-24 05:06:55 +0000

    First commit

---
+++ src/main/java/org/chahid/challengetech/ChallengeTechApplication.java
@@ -0,0 +1,13 @@
+package org.chahid.challengetech;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ChallengeTechApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(ChallengeTechApplication.class, args);
+    }
+
+}
---
+++ src/test/java/org/chahid/challengetech/ChallengeTechApplicationTests.java
@@ -0,0 +1,13 @@
+package org.chahid.challengetech;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ChallengeTechApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
